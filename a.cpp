024deb4cc8e3ff7035c41199427b580e49e87e19
rdb1377 <rdb1377@gmail.com>
2016-12-13 14:53:12 +0330

    hello world

--- a.cpp
+++ a.cpp
@@ -1,9 +1,20 @@
 #include<stdio.h>
-
+int isprime (int i)
+{
+	int e;
+	for (e=0;e<i;e++)
+	{
+		if (i%e!=0)
+		return 0;
+		else 
+		return 1;
+	}
+}
 
 int main(){
 	int n;
 	scanf("%d",&n);
 	printf("%d",n+1);
+	printf ("%d",isprime (n));
 	return 0;
 }
